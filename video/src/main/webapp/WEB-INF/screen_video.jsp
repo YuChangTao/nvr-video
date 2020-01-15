ca4d3a3bf4d8843ec6b9b7dacfc33769e44237f5
bme_yutao <2686222074@qq.com>
2020-01-15 15:38:18 +0800

    样式调整

--- video/src/main/webapp/WEB-INF/screen_video.jsp
+++ video/src/main/webapp/WEB-INF/screen_video.jsp
@@ -21,7 +21,11 @@
     <link rel="stylesheet" type="text/css" href="../static/page/css/base.css"/>
     <link rel="stylesheet" type="text/css" href="../static/page/css/common.css"/>
     <link href="../static/video/video-js.min.css" rel="stylesheet">
-
+    <!--引入播放器样式-->
+    <link href="http://vjs.zencdn.net/5.19/video-js.min.css" rel="stylesheet">
+    <!--引入播放器js-->
+    <script src="http://vjs.zencdn.net/5.19/video.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/videojs-flash@2/dist/videojs-flash.min.js"></script>
 </head>
 
 <body>
@@ -86,7 +90,7 @@
 <script src="../static/layui/layui.js" type="text/javascript"></script>
 <%--<script type="text/javascript" src="../static/video/video.min.js"></script>--%>
 <%--<script type="text/javascript" src="../static/video/videojs-flash.min.js"></script>--%>
-<script type="text/javascript" src="../static/video/easy-player-element.min.js"></script>
+<%--<script type="text/javascript" src="../static/video/easy-player-element.min.js"></script>--%>
 <script src="../static/page/js/index.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
     layui.use(['form', 'layedit', 'laydate'], function() {
--- video/src/main/webapp/static/page/js/index.js
+++ video/src/main/webapp/static/page/js/index.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-	 function countScale() {
+    function countScale() {
         var sc = $(window).width() / 1920;
         //var sch = $(window).height();
         $('body').css("transform", "scale(" + sc + ")")
         /*$('body').css("transform", "scale(" + sch + ")")*/
-       // console.log(sc);
-       //console.log(sch);
+        // console.log(sc);
+        //console.log(sch);
 
 
     }
@@ -18,7 +18,8 @@ $(document).ready(function(){
     getConfig();
 
 })
-
+layui.use(['layer', 'form'], function(){
+});
 
 // var baseUrl = "http://demo.easynvr.com:10800";
 //裕华
@@ -37,6 +38,15 @@ var timerArr=[];
 var config;
 //延时触发单双击
 var clickTimeSingle,clickTimeDouble;
+//
+var customerId=GetQueryString("customerId");
+var flvOrRtmp=GetQueryString("customerId") == 6?"RTMP":"FLV";
+if(customerId != 6){
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "../static/video/easy-player-element.min.js";
+	document.getElementsByTagName("head")[0].appendChild(script);
+}
 //li索引
 //var index;
 // videojs.options.flash.swf = '../static/video/video-js.swf';
@@ -54,6 +64,8 @@ function login() {
 
 function getConfig() {
     // var getUrl = '/nvr/config/classify';
+//  wjp
+//  getChannel();
     var getUrl = 'http://wx.bmetech.com:9091/platform/eq/url';
     $.get(getUrl, {customerId:GetQueryString("customerId")}, function (res) {
         if (res) {
@@ -84,41 +96,42 @@ function getConfig() {
  */
 function getChannel() {
     var getUrl = baseUrl + '/api/v1/getchannels';
-
-    /*    var res = '{\n' +
-            '    "EasyDarwin": {\n' +
-            '        "Header": {\n' +
-            '            "CSeq": "1",\n' +
-            '            "Version": "v1",\n' +
-            '            "MessageType": "MSG_SC_SERVER_GET_CHANNELS_ACK",\n' +
-            '            "ErrorNum": "200",\n' +
-            '            "ErrorString": "Success OK"\n' +
-            '        },\n' +
-            '        "Body": {\n' +
-            '            "ChannelCount": 8,\n' +
-            '            "Channels": [\n' +
-            '                {\n' +
-            '                    "Channel": 1,\n' +
-            '                    "Name": "Channel1",\n' +
-            '                    "Online": 1,\n' +
-            '                    "SnapURL": "/snap/1/channel_1.jpg?t=1558600547089659600",\n' +
-            '                    "ErrorString": ""\n' +
-            '                },\n' +
-            '                {\n' +
-            '                    "Channel": 16,\n' +
-            '                    "Name": "Channel16",\n' +
-            '                    "Online": 0,\n' +
-            '                    "SnapURL": "/snap/16/channel_16.jpg",\n' +
-            '                    "ErrorString": ""\n' +
-            '                }\n' +
-            '            ]\n' +
-            '        }\n' +
-            '    }\n' +
-            '}\n';
-        res = JSON.parse(res);
-        var list = res.EasyDarwin.Body.Channels;
-        data = list;
-        createView(list);*/
+//wjp
+//      var res = '{\n' +
+//          '    "EasyDarwin": {\n' +
+//          '        "Header": {\n' +
+//          '            "CSeq": "1",\n' +
+//          '            "Version": "v1",\n' +
+//          '            "MessageType": "MSG_SC_SERVER_GET_CHANNELS_ACK",\n' +
+//          '            "ErrorNum": "200",\n' +
+//          '            "ErrorString": "Success OK"\n' +
+//          '        },\n' +
+//          '        "Body": {\n' +
+//          '            "ChannelCount": 8,\n' +
+//          '            "Channels": [\n' +
+//          '                {\n' +
+//          '                    "Channel": 1,\n' +
+//          '                    "Name": "Channel1",\n' +
+//          '                    "Online": 1,\n' +
+//          '                    "SnapURL": "/snap/1/channel_1.jpg?t=1558600547089659600",\n' +
+//          '                    "ErrorString": ""\n' +
+//          '                },\n' +
+//          '                {\n' +
+//          '                    "Channel": 16,\n' +
+//          '                    "Name": "Channel16",\n' +
+//          '                    "Online": 0,\n' +
+//          '                    "SnapURL": "/snap/16/channel_16.jpg",\n' +
+//          '                    "ErrorString": ""\n' +
+//          '                }\n' +
+//          '            ]\n' +
+//          '        }\n' +
+//          '    }\n' +
+//          '}\n';
+//      res = JSON.parse(res);
+//      var list = res.EasyDarwin.Body.Channels;
+//      data = list;
+//      createView(list);
+       
     $.get(getUrl, {}, function (res) {
         if (res.EasyDarwin.Header.ErrorNum == 200) {
             var list = res.EasyDarwin.Body.Channels;
@@ -160,10 +173,12 @@ function getChannelByType(type) {
  * 根据通道获取直播流地址
  */
 function getChannelStream(index) {
+//	rtmpUrl = "rtmp://demo.easynvr.com:10935/hls/stream_9?token=b8c45f4ecd6232baaae05d1f00183a07"
     $.ajaxSettings.async = false;//设置为同步
     var getUrl = baseUrl + '/api/v1/getchannelstream';
-    $.get(getUrl, {"channel": channelIdArr[index],"protocol":"FLV"}, function (res) {
+    $.get(getUrl, {"channel": channelIdArr[index],"protocol":flvOrRtmp}, function (res) {
         if (res.EasyDarwin.Header.ErrorNum == 200) {
+//      	wjp
             rtmpUrl = res.EasyDarwin.Body.URL;
         } else {
             rtmpUrl = "";
@@ -187,7 +202,8 @@ function createView(list) {
             '          <div class="right_bottom"></div>' +
             '          <div class="img_flow_chart_div">' +
             '                <div class="img_box_flow">' +
-            '                      <img src="'+baseUrl + list[i].SnapURL+'" width="100%" class="gy_pic_img" />' +
+            //wjp
+              '                      <img src="'+baseUrl + list[i].SnapURL+'" width="100%" class="gy_pic_img" />' +
             '                      <div class="bg_play_img"></div>' +
             '                </div>' +
             '                <p class="clearfix violation_img_time">' +
@@ -221,18 +237,29 @@ function initLiEvent(val) {
             //获取通道流
             var index = $(_this).index();
             var channel = data[index];
+            var myPlayer="my-player"+(Math.random()*1000).toFixed(0);
             console.log(data[index]);
             channelIdArr[index] = channel.Channel;
             getChannelStream(index);
             $.ajaxSettings.async = true;
-
-            var videoStr = '<easy-player id="my-video" live="true" aspect="187:100" show-custom-button="true" video-url="'+rtmpUrl+'"></easy-player>'
+			if(customerId == 6){
+				//普阳
+				var videoStr = '<video autoplay="autoplay" id="'+myPlayer+'" class="video-js vjs-default-skin vjs-big-play-centered" controls preload="auto"'
+							    +'  poster="" width="1000" height="500" data-setup="{}">'
+							    +'<source src="'+rtmpUrl+'"  type="rtmp/flv"/>'
+							+'</video>'
+			}else{
+				var videoStr = '<easy-player id="my-video" live="true" aspect="187:100" show-custom-button="true" video-url="'+rtmpUrl+'"></easy-player>'
+	            
+			}
             $("#videoBox").html(videoStr);
-            $(".videoBox").find(".vjs-control-bar").hide();//隐藏底部工具
+            let player = videojs(myPlayer); //my-player为页面video元素的id
+    			player.play();
+	        $(".videoBox").find(".vjs-control-bar").hide();//隐藏底部工具
             //定时刷新心跳
             let refreshCountFun=function(){
                 var getUrl = baseUrl + "/api/v1/touchchannelstream";
-                $.get(getUrl, {channel: channelIdArr[index], protocol: "rtmp","_":new Date().getTime()}, function (res) {
+                $.get(getUrl, {channel: channelIdArr[index], protocol: flvOrRtmp,"_":new Date().getTime()}, function (res) {
                 })
             };
             timerArr[index] = window.setInterval(refreshCountFun, 5000);
@@ -266,33 +293,46 @@ function initLiEvent(val) {
     $(document).on("click",".violation_body_li",function(){
         let _this=this;
         var index = $(_this).index();
-        if(!$(this).find("video").length){
+        if(!$(this).find(".videoS").length){
             clearTimeout(clickTimeSingle);
             clickTimeSingle = setTimeout(function(){
                 //获取通道流
 //		        index = $(_this).index();
                 var channel = data[index];
+                var myPlayer="my-player"+(Math.random()*1000).toFixed(0);
                 console.log(index);
                 channelIdArr[index] = channel.Channel;
                 getChannelStream(index);
                 $.ajaxSettings.async = true;
-                var videoStr = '<easy-player id="my-video" live="true" aspect="150:100" show-custom-button="true" video-url="'+rtmpUrl+'"></easy-player><div class="video_bg"></div>'
                 $(_this).find("img,.bg_play_img").hide();
+                if(customerId == 6){
+					//普阳
+					var videoStr = '<video autoplay="autoplay" id="'+myPlayer+'" class="videoS video-js vjs-default-skin vjs-big-play-centered" controls preload="auto"'
+								    +'  poster="" width="400" height="299" data-setup="{}">'
+								    +'<source src="'+rtmpUrl+'"  type="rtmp/flv"/>'
+								+'</video><div class="video_bg"></div>';
+				}else{
+					var videoStr = '<easy-player id="my-video" class="videoS" live="true" aspect="150:100" show-custom-button="true" video-url="'+rtmpUrl+'"></easy-player><div class="video_bg"></div>'
+				}
+				$(_this).find("img,.bg_play_img").hide();
                 $(_this).find(".img_box_flow").append(videoStr);
+                let player = videojs(myPlayer); //my-player为页面video元素的id
+    			player.play();
                 $(_this).find(".vjs-control-bar").hide();//隐藏底部工具
                 //定时刷新心跳
                 console.log(index)
                 let refreshCountFun=function(){
                     var getUrl = baseUrl + "/api/v1/touchchannelstream";
-                    $.get(getUrl, {channel: channelIdArr[index], protocol: "rtmp","_":new Date().getTime()}, function (res) {
+                    $.get(getUrl, {channel: channelIdArr[index], protocol: flvOrRtmp,"_":new Date().getTime()}, function (res) {
                     })
                 };
                 timerArr[index] = window.setInterval(refreshCountFun, 5000);
             },250)
-        }else{
+      }else{
             $(_this).find("img,.bg_play_img").show();
             $(_this).find("easy-player").remove();
             $(_this).find("video").remove();
+            $(_this).find(".video-js").remove();
             window.clearInterval(timerArr[index]);
         }
 
@@ -308,7 +348,7 @@ function initLiEvent(val) {
 function refreshCount(index) {
 //	alert('123')
     var getUrl = baseUrl + "/api/v1/touchchannelstream";
-    $.get(getUrl, {channel: channelIdArr[index], protocol: "FLV","_":new Date().getTime()}, function (res) {
+    $.get(getUrl, {channel: channelIdArr[index], protocol: flvOrRtmp,"_":new Date().getTime()}, function (res) {
 
     })
 }
